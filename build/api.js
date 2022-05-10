3844e44009abcd41c3c7526452dd27bbf82871b1
Ruslan B <74817877+rusbers@users.noreply.github.com>
2022-05-10 21:02:37 +0100

    Delete build directory

--- build/api.js
+++
@@ -1,7 +0,0 @@
-const API = {
-    GENDERIZE: 'https://api.genderize.io',
-};
-function generateServerUrl(name) {
-    return `${API.GENDERIZE}?name=${name}`;
-}
-export { generateServerUrl };
--- build/view.js
+++
@@ -1,6 +0,0 @@
-const UI_ELEMENTS = {
-    FORM: document.querySelector('.form'),
-    INPUT: document.querySelector('.input'),
-    OUTPUT: document.querySelector('.output'),
-};
-export { UI_ELEMENTS };
